1799ffa7f51ac6aeeb8f586a194cadfafec51fac
Miles-Kurth <mkurth9612@student.pps.net>
2026-05-12 15:28:21 -0700

    yep

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 from pybricks.iodevices import I2CDevice
 from pybricks.parameters import Color
+import time
 
 
 # Initialize the EV3 Brick.
@@ -24,14 +25,22 @@ right_sensor = ColorSensor(Port.S1)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
 # Declare Variables
-speed_left = 0
-speed_right = 0
+vel_reft = 0
+vel_right = 0
 
 # Functions
 
-def set_speed(self, speed)
-    self.run(speed)
-Motor.set_speed = set_speed
+def left_check():
+    if left_sensor.reflection() > 8:
+        return False
+    else:
+        return True
+
+def right_check():
+    if right_sensor.reflection() > 8:
+        return False
+    else:
+        return True
 
 
 # At start
@@ -41,10 +50,11 @@ ev3.speaker.beep(440)
 
 # CODE BELOW
 
-vel_Left = 500
-vel_Right = 500
+vel_reft = 500
+vel_right = 500
 
 while True:
-    left_motor.run(vel_Left)
-    right_motor.run(vel_Right)
-    #color > 10 or so
+    # left_motor.run(vel_Left)
+    # right_motor.run(vel_Right)
+    if left_check():
+        print(str(time.time()) + " " + str(left_sensor.reflection()))
