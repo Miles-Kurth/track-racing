abc7e94755b1f058f93df74c0621f6ed04d180dd
Miles-Kurth <mkurth9612@student.pps.net>
2026-05-14 16:59:48 -0700

    aaaaaaaa  WHY

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ left_motor = Motor(Port.B)
 right_motor = Motor(Port.D)
 
 # Initialize sensors
-left_sensor = ColorSensor(Port.S4)
-right_sensor = ColorSensor(Port.S1)
+left_sensor = ColorSensor(Port.S1)
+right_sensor = ColorSensor(Port.S4)
 center_sensor = ColorSensor(Port.S3)
 
 # Initialize drivebase
@@ -29,23 +29,29 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 # Declare Variables
 vel_left = 0
 vel_right = 0
-MAX_TOTAL = 1000
+START_SPEED = 300
 last_side = "between" # left, right
 
 
 # Functions
+
+def get_sign(num):
+    if num < 0:
+        return -1
+    if num >= 0:
+        return 1
 
 def update_motors():
     update_left()
     update_right()
 
 def update_left():
-    global vel_left
-    left_motor.run(vel_left)
+    global vel_left; global START_SPEED
+    left_motor.run(START_SPEED + vel_left)
 
 def update_right():
-    global vel_right
-    right_motor.run(vel_right)
+    global vel_right; global START_SPEED
+    right_motor.run(START_SPEED + vel_right)
 
 
 def left_check():
@@ -72,32 +78,32 @@ def center_check():
 
 def fix_deviation():
     global vel_left; global vel_right; global last_side
+
     if not (left_check() or right_check() or center_check()): # off the track
-        if last_side == "left": # turning left
-            vel_right = 600; vel_left = 400
-        if last_side == "right": #turning right
-            vel_left = 600; vel_right = 400
+        vel_left *= 1.1; vel_right *= 1.1
+    
+    if center_check():
+        vel_left *= 0.8; vel_right *= 0.8
 
 def fix_vels():
     global vel_left; global vel_right
 
     vel_left = round(vel_left); vel_right = round(vel_right)
-    total = vel_left + vel_right
+    if abs(vel_left) > 100:
+        vel_left = 100 * get_sign(vel_left)
+    if abs(vel_right) > 100:
+        vel_right = 100 * get_sign(vel_right)
 
-    
+    total = vel_left + vel_right
     difference = abs(vel_left - vel_right)
 
     if not (left_check() or right_check()) and center_check(): # directly on line
-        vel_left = 500; vel_right = 500
-
+        vel_left = 0; vel_right = 0
+        
+    
     # bring closer to straight
-    if center_check():
-        difference *= 1.5
-    if difference > 200:
-        if vel_left > vel_right:
-            vel_left -= (difference / 5); vel_right += (difference / 5)
-        if vel_right > vel_left:
-            vel_right -= (difference / 5); vel_left += (difference / 5)
+    # if difference <= 200:
+        
     
 
 
@@ -110,24 +116,29 @@ ev3.speaker.beep(440)
 
 # CODE BELOW
 
-vel_left = 300
-vel_right = 300
+vel_left = 0
+vel_right = 0
+
+
 
 update_motors()
 while True:
-    if left_check():
-        vel_right += -1
-        vel_left -= -1
+    if left_check(): # turn left
+        vel_right += 1
+        vel_left += -1
         fix_vels()
         update_motors()
-    if right_check():
-        vel_left += -1
-        vel_right -= -1
+    
+    if right_check(): # turn right
+        vel_left += 1
+        vel_right += -1
         fix_vels()
         update_motors()
     fix_deviation()
 
-    print("Total = " + str(vel_left + vel_right) + ",  ", end="")
-    print("L = " + str(vel_left) + str(left_check()) + "  ", end="")
-    print("R = " + str(vel_right) + str(right_check()))
+
+    # print info
+    print("Total = " + str(vel_left + vel_right) + ",   ", end="") # total speed
+    print("L = " + str(vel_left) + str(left_check()) + "  ", end="") # LEFT speed & [sees line?]
+    print("R = " + str(vel_right) + str(right_check())) # RIGHT speed & [sees line?]
     
